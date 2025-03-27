29647dbb5350272732d18ac5659a753ba9e1560c
choi-jae-been <cotjsqls04@naver.com>
2025-03-27 16:07:37 +0900

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -3,7 +3,7 @@ echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
 yum install -y iptables-services
 systemctl start iptables.service
-systmectl enable iptables.service
+systemctl enable iptables.service
 iptables -F
 iptables -t nat -A POSTROUTING -o ethO -j MASQUERADE  
 service iptables save
